4abea365267c15a386f766432deb54705f59f288
alecthedev <alecthedev@proton.me>
2025-08-28 14:20:25 -0400

    add test for fibonacci negative input

--- test/test.js
+++ test/test.js
@@ -12,3 +12,9 @@ describe('First two terms', function() {
       assert.strictEqual(main.fibonacci(1), 1);
     });
 });
+
+describe('Negative number: -10', function() {
+  it('should return undefined for negative numbers', function() {
+      assert.strictEqual(main.fibonacci(-10), undefined);
+    });
+});
